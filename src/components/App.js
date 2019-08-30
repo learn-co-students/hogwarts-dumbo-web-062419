33e0ce65c31b0fb3a812c857319f783ffda227bd
Dennis Basyrov <dennis.basyrov@gmail.com>
2019-08-30 16:51:10 -0400

    Randy way

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,38 @@ import React, { Component } from 'react';
 import '../App.css';
 import Nav from './Nav'
 import hogs from '../porkers_data';
+import HogContainer from "./HogContainer";
+
 
 class App extends Component {
+
+  state = {
+    allHogs: hogs,
+    onlyGreased: false
+  };
+
+
+  filterByGrease = () => {
+    this.setState({ onlyGreased: !this.state.onlyGreased });
+  };
+
   render() {
+    let filteredHogs = [...this.state.allHogs];
+    console.log('greased: ', this.state.onlyGreased)
+    if (this.state.onlyGreased) {
+      filteredHogs = filteredHogs.filter(hog => {
+        return hog.greased === true;
+      });
+    }
+    console.log(filteredHogs)
+
+
     return (
       <div className="App">
-          < Nav />
-
+          < Nav
+          filterByGrease={this.filterByGrease}
+          />
+          < HogContainer allHogs={filteredHogs} />
       </div>
     )
   }
