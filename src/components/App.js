baf3dffc9bdbbf222c112441e7013c2fd43a4a00
Dennis Wang <dwang0816@gmail.com>
2019-09-18 17:23:51 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,52 @@ import React, { Component } from 'react';
 import '../App.css';
 import Nav from './Nav'
 import hogs from '../porkers_data';
+import Hogtiles from './Hogtiles'
 
 class App extends Component {
+
+  state = {
+    allHogs:[],
+    onlyGreased: false,
+    
+  };
+
+  componentDidMount(){
+
+    this.setState({allHogs: hogs})
+ 
+  }
+
+  // componentDidUpdate(prevProps, prevState){
+  //   debugger
+  //   if (prevState.allHogs !== this.props.allHogs) {
+  //     console.log("hi")
+  //   }
+  // }
+
+  filterByGrease = () => {
+    this.setState({ onlyGreased: !this.state.onlyGreased})
+  }
+
+
+
   render() {
+      let filteredHogs = [...this.state.allHogs]
+      
+      if (this.state.onlyGreased === true) {
+        filteredHogs = filteredHogs.filter(hog => {
+          return hog.greased 
+        })
+      }
+
     return (
-      <div className="App">
-          < Nav />
+      <div className="ui grid container App">
+        <div className="sixteen wide column centered">
+          < Nav filterByGrease ={this.filterByGrease}/>
+        </div>
+        <div className="sixteen wide column centered">
+          <Hogtiles allHogs={filteredHogs} />
+        </div>
 
       </div>
     )
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,37 @@
 import piggy from '../porco.png'
-import React from 'react'
+import React, { Component } from 'react'
 
-const Nav = () => {
-	return (
-		<div className="navWrapper">
-			<span className="headerText">Hogwarts</span>
-			<div className="TwirlyPig">
-				<a href="https://www.lowes.com/pd/LG-24-7-cu-ft-French-Door-Refrigerator-with-Ice-Maker-Stainless-steel/4746231">
-					<img src={piggy} className="App-logo" alt="piggy" />
-				</a>
+class Nav extends Component {
+	render() { 
+		return (
+			<div className="navWrapper">
+				<span className="headerText" onClick={this.refresh}>Hogwarts</span>
+				<div className="TwirlyPig">
+					<a href="https://www.lowes.com/pd/LG-24-7-cu-ft-French-Door-Refrigerator-with-Ice-Maker-Stainless-steel/4746231">
+						<img src={piggy} className="App-logo" alt="piggy" />
+					</a>
+				</div>
+				<span className="normalText">A React App for County Fair Hog Fans</span>
+				<button onClick={() => this.props.filterByGrease()}>Filter By Grease</button>
 			</div>
-			<span className="normalText">A React App for County Fair Hog Fans</span>
-		</div>
-	)
+		);
+	}
 }
+ 
+export default Nav;
 
-export default Nav
+// const Nav = () => {
+// 	return (
+// 		<div className="navWrapper">
+// 			<span className="headerText">Hogwarts</span>
+// 			<div className="TwirlyPig" onClick {}>
+// 				<a href="https://www.lowes.com/pd/LG-24-7-cu-ft-French-Door-Refrigerator-with-Ice-Maker-Stainless-steel/4746231">
+// 					<img src={piggy} className="App-logo" alt="piggy" />
+// 				</a>
+// 			</div>
+// 			<span className="normalText">A React App for County Fair Hog Fans</span>
+// 		</div>
+// 	)
+// }
+
+// export default Nav
